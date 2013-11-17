3113f2efcdc6e631575880ac29d296ef3aac815f
Jim Rosser <jarosser06@gmail.com>
2013-11-16 20:35:43 -0600

    support for setting config options

--- lib/dhcpdash.rb
+++ lib/dhcpdash.rb
@@ -2,6 +2,23 @@ require_relative 'dhcpdash/network'
 require_relative 'dhcpdash/host'
 
 module DHCPDash
+  extend self
+
+  def parameter(*names)
+    names.each do |name|
+      attr_accessor name
+
+      define_method name do |*values|
+        value = values.first
+        value ? self.send("#{name}=", value) : instance_variable_get("@#{name}")
+      end
+    end
+  end
+
+  def config(&block)
+    instance_eval &block
+  end
+
   def network_exists?(id)
     File.exists?("./networks/#{id}.json")
   end
