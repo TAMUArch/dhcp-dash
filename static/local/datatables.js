c8744912080fcc147fb342e99250edc0d285ff49
Victoria Blessing <vblessing@arch.tamu.edu>
2013-12-09 17:00:53 -0600

    started formatting fix

--- static/local/datatables.js
+++ static/local/datatables.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  $('#hosts').dataTable();
+  $('#hosts').dataTable( {
+    "sDom": "<'row'<'span8'l><'span8'f>r>t<'row'<'span8'i><'span8'p>>"
+    "sPaginationType": "bootstrap"
+  } );
 } );
---
+++ static/local/tooltips.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $("[rel='addNetworkTooltip']").tooltip({placement: 'left'});
+});
